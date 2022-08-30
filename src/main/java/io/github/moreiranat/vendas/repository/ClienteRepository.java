96db4d4d99e5dbeccdf041135556d4c8a3d887f6
Nataly Lucena <natalylucena.pb@gmail.com>
2022-08-30 13:51:34 -0300

    Creating the configuration class and injecting dependency on the ClienteService class

---
+++ src/main/java/io/github/moreiranat/vendas/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package io.github.moreiranat.vendas.repository;
+
+import io.github.moreiranat.vendas.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base de dados e salva o cliente
+    }
+}
