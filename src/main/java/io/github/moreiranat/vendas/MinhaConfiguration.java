6a3d8907702a937b6b24360ac68d3ae080ccb666
Nataly Lucena <natalylucena.pb@gmail.com>
2022-08-30 20:19:57 -0300

    Creating custom annotation for Developmet

--- src/main/java/io/github/moreiranat/vendas/MinhaConfiguration.java
+++ src/main/java/io/github/moreiranat/vendas/MinhaConfiguration.java
@@ -2,11 +2,8 @@ package io.github.moreiranat.vendas;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MinhaConfiguration {
 
     @Bean
