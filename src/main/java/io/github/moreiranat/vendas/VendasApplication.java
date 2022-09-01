7c1ed72e47ee54f2da7d3df3df79efb65cad6cf8
Nataly Lucena <natalylucena.pb@gmail.com>
2022-08-31 23:28:18 -0300

    Updating, Deleting and Searching for Clients

--- src/main/java/io/github/moreiranat/vendas/VendasApplication.java
+++ src/main/java/io/github/moreiranat/vendas/VendasApplication.java
@@ -16,11 +16,35 @@ public class VendasApplication {
 	@Bean
 	public CommandLineRunner init (@Autowired Clientes clientes) {
 		return args -> {
+			System.out.println("Salvando Clientes");
 			clientes.salvar(new Cliente("Nataly"));
 			clientes.salvar(new Cliente("Outro Cliente"));
 
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
+
+			System.out.println("Atualizando Clientes");
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado.");
+				clientes.atualizar(c);
+			});
+
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+			System.out.println("Buscando Clientes");
+			clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+			System.out.println("Deletando Clientes");
+			clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c);
+			});
+			todosClientes = clientes.obterTodos();
+			if(todosClientes.isEmpty()) {
+				System.out.println("Nenhum cliente encontrado.");
+			} else {
+				todosClientes.forEach(System.out::println);
+			}
 		};
 	}
 
--- src/main/java/io/github/moreiranat/vendas/domain/repositorio/Clientes.java
+++ src/main/java/io/github/moreiranat/vendas/domain/repositorio/Clientes.java
@@ -3,6 +3,7 @@ package io.github.moreiranat.vendas.domain.repositorio;
 import io.github.moreiranat.vendas.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
@@ -10,11 +11,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import static org.hibernate.loader.Loader.SELECT;
+
 @Repository
 public class Clientes { //são todos os Clientes
 
     private static String INSERT = "insert into cliente (nome) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
+    private static String UPDATE = "update cliente set nome = ? where id = ? ";
+    private static String DELETE = "delete from cliente where id = ? ";
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -23,14 +29,41 @@ public class Clientes { //são todos os Clientes
         return cliente;
     }
 
+    public Cliente atualizar (Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    //deletar passando o cliente
+    public void deletar(Cliente cliente) {
+        deletar(cliente.getId());
+    }
+
+    //deletar passando o id
+    public void deletar(Integer id) {
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome) {
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
     public List<Cliente> obterTodos() {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 }
