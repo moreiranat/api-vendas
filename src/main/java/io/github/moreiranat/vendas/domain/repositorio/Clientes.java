294ccac03bbaafe872c5596487db5aeb7932e80d
Nataly Lucena <natalylucena.pb@gmail.com>
2022-08-31 23:53:32 -0300

    Persisting Entities with Entity Manager

--- src/main/java/io/github/moreiranat/vendas/domain/repositorio/Clientes.java
+++ src/main/java/io/github/moreiranat/vendas/domain/repositorio/Clientes.java
@@ -1,12 +1,15 @@
 package io.github.moreiranat.vendas.domain.repositorio;
 
 import io.github.moreiranat.vendas.domain.entity.Cliente;
+import org.hibernate.EntityMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,7 +19,6 @@ import static org.hibernate.loader.Loader.SELECT;
 @Repository
 public class Clientes { //são todos os Clientes
 
-    private static String INSERT = "insert into cliente (nome) values (?) ";
     private static String SELECT_ALL = "SELECT * FROM CLIENTE ";
     private static String UPDATE = "update cliente set nome = ? where id = ? ";
     private static String DELETE = "delete from cliente where id = ? ";
@@ -24,8 +26,12 @@ public class Clientes { //são todos os Clientes
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
     public Cliente salvar (Cliente cliente) {
-        jdbcTemplate.update(INSERT, new Object[] {cliente.getNome()});
+        entityManager.persist(cliente);
         return cliente;
     }
 
