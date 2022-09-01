22aba135a1565d92e6a384128388aebb55ed9133
Nataly Lucena <natalylucena.pb@gmail.com>
2022-08-31 22:34:23 -0300

    Saving and retrieving customers

--- src/main/java/io/github/moreiranat/vendas/VendasApplication.java
+++ src/main/java/io/github/moreiranat/vendas/VendasApplication.java
@@ -1,13 +1,30 @@
 package io.github.moreiranat.vendas;
 
+import io.github.moreiranat.vendas.domain.entity.Cliente;
+import io.github.moreiranat.vendas.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
 
+	@Bean
+	public CommandLineRunner init (@Autowired Clientes clientes) {
+		return args -> {
+			clientes.salvar(new Cliente("Nataly"));
+			clientes.salvar(new Cliente("Outro Cliente"));
+
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
-
 }
