c6d29513bd65f273d8f648d8f5fc2f2240a79cf1
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-02 17:20:19 -0300

    Loading orders by customer

--- src/main/java/io/github/moreiranat/vendas/domain/repository/Pedidos.java
+++ src/main/java/io/github/moreiranat/vendas/domain/repository/Pedidos.java
@@ -1,7 +1,11 @@
 package io.github.moreiranat.vendas.domain.repository;
 
+import io.github.moreiranat.vendas.domain.entity.Cliente;
 import io.github.moreiranat.vendas.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
+    List<Pedido> findByCliente(Cliente cliente);
 }
