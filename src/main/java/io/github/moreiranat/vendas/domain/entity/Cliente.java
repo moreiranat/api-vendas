349887258a9861128db8013bbddcf3c90b49cdc0
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-03 17:37:48 -0300

    Customer Search by parameters

--- src/main/java/io/github/moreiranat/vendas/domain/entity/Cliente.java
+++ src/main/java/io/github/moreiranat/vendas/domain/entity/Cliente.java
@@ -16,6 +16,9 @@ public class Cliente {
     @Column(name = "nome", length = 100)
     private String nome;
 
+    @Column(name = "cpf", length = 11)
+    private String cpf;
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos; //pode colocar Collection<Pedido> pedidos
@@ -47,6 +50,14 @@ public class Cliente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public Set<Pedido> getPedidos() {
