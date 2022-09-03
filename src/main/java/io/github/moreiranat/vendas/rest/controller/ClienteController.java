4e0006ce654e0b405cf3dffd49bd4ec20c85a33f
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-03 00:46:59 -0300

    Mapping Requests with Request Mapping

--- src/main/java/io/github/moreiranat/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/github/moreiranat/vendas/rest/controller/ClienteController.java
@@ -1,18 +1,20 @@
 package io.github.moreiranat.vendas.rest.controller;
 
+import io.github.moreiranat.vendas.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/api/clientes") //url base da api de clientes
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/api/clientes/hello/{nome}", "/api/hello"},
+            method = RequestMethod.POST,
+            consumes = {"apllication/json", "application/xml"},
+            produces = {"apllication/json", "application/xml"}
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, RequestBody Cliente cliente) {
         return String.format("Hello %s ", nomeCliente);
     }
 }
