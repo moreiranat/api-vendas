5691879467f0413438129fba01fdf166ddbec76f
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-04 00:39:16 -0300

    Using the Lombok Project

--- src/main/java/io/github/moreiranat/vendas/domain/entity/Produto.java
+++ src/main/java/io/github/moreiranat/vendas/domain/entity/Produto.java
@@ -1,8 +1,13 @@
 package io.github.moreiranat.vendas.domain.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "produto")
 public class Produto {
@@ -16,27 +21,4 @@ public class Produto {
     @Column(name = "preco_unitario")
     private BigDecimal preco;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
 }
--- src/main/java/io/github/moreiranat/vendas/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/moreiranat/vendas/rest/dto/PedidoDTO.java
@@ -1,8 +1,15 @@
 package io.github.moreiranat.vendas.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PedidoDTO { //DTO: Data Transfer Object - padrao que serve para mapear um objeto com propriedades simples
 
     private Integer cliente; //id do cliente
