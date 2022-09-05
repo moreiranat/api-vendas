708a61319884d6064e92d8550158cdd33a2838bf
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-05 16:50:06 -0300

    Basic authentication

--- src/main/java/io/github/moreiranat/vendas/config/SecurityConfig.java
+++ src/main/java/io/github/moreiranat/vendas/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/produtos/**")
                         .hasRole("ADMIN")
                 .and()
-                    .formLogin();
+                    .httpBasic();
+        ;
     }
 }
