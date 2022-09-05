df45e7aecd4e8754998060dbae76c32e28fc3aa3
Nataly Lucena <natalylucena.pb@gmail.com>
2022-09-05 11:22:03 -0300

    Validating the Order entity

--- src/main/java/io/github/moreiranat/vendas/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/moreiranat/vendas/rest/dto/PedidoDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -12,7 +13,11 @@ import java.util.List;
 @AllArgsConstructor
 public class PedidoDTO { //DTO: Data Transfer Object - padrao que serve para mapear um objeto com propriedades simples
 
+    @NotNull(message = "Informe o código do cliente.")
     private Integer cliente; //id do cliente
+
+    @NotNull(message = "Campo total do pedido é obrigatório.")
     private BigDecimal total;
+
     private List<ItemPedidoDTO> itens;
 }
